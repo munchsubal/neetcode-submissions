aed583d724a9c8e479275b6ec0c51e05b7baa8bd
Subal Manchanda <subal.manchanda@gmail.com>
2026-05-30 16:39:49 +0530

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int findRow(vector<vector<int>>& matrix, int target, int m, int low, int high) {
+        if (low > high) {
+            return false;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (matrix[mid][0] <= target && target <= matrix[mid][m - 1]) {
+            return mid;
+        }
+       
+        if (target < matrix[mid][0]) {
+            return findRow(matrix, target, m, low, mid-1);
+        }
+        else {
+            return findRow(matrix, target, m, mid+1, high);
+        }
+
+        return -1;
+    }
+
+    bool findNum(vector<int>& nums, int target, int low, int high) {
+        if (low > high) {
+            return false; 
+        }
+        
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) {
+            return true;
+        }
+        
+        if (nums[mid] > target) {
+            return findNum(nums, target, low, mid - 1);
+        }
+        else {
+            return findNum(nums, target, mid + 1, high);
+        }
+    
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int row = findRow(matrix, target, n, 0, m-1);
+        cout << row << endl;
+
+        if (row == -1) {
+            return false;
+        }
+        return findNum(matrix[row], target, 0, n-1);
+    }
+};
