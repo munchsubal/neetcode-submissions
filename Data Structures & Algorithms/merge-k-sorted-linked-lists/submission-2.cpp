847ee8aeacff1d8469d7aaa20ce3f9f7c2f4d2a6
Subal Manchanda <subal.manchanda@gmail.com>
2026-06-18 10:49:24 +0530

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.cpp
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        ListNode* dummyNode = new ListNode(-1);
+        dummyNode->next = nullptr;
+        ListNode* prev = dummyNode;
+
+        if (k == 0) {
+            return nullptr;
+        }
+        if (k == 1) {
+            return lists[0];
+        }
+
+        int cnt_null = 0;
+        for (int i=0; i<k; i++) {
+            if (lists[i] == nullptr) {
+                cnt_null++;
+            }
+        }
+
+        if (cnt_null == k) {
+            return nullptr;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for (int i=0; i<k; i++) {
+            if (lists[i] != nullptr) {
+                pq.push({lists[i]->val, i});
+            }
+        }
+
+        while (true) {
+            int minIdx = pq.top().second;
+            ListNode* minValNode = lists[pq.top().second];
+            pq.pop();
+            prev->next = minValNode;
+            prev = minValNode;
+
+            lists[minIdx] = lists[minIdx]->next;
+            if (lists[minIdx] == nullptr) {
+                cnt_null++;
+            }
+            else {
+                pq.push({lists[minIdx]->val, minIdx});
+            }
+
+            if (cnt_null == k) {
+                break;
+            }
+        }
+
+        return dummyNode->next;
+    }
+};
