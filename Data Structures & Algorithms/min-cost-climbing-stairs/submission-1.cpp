ef8e516feb8bdce8864af5c3c5c28ee807bf338a
Subal Manchanda <subal.manchanda@gmail.com>
2026-07-09 07:00:00 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int prev = 0;
+        int prev2 = 0;
+        int curr = 0;
+
+        for (int i=2; i<=n; i++) {
+            curr = min(prev + cost[i - 1], prev2 + cost[i - 2]);
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return curr;
+    }
+};
