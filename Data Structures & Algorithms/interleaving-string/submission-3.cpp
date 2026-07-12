09ef4a6f0f91b582d88d07e53c5617139b4d8f33
Subal Manchanda <subal.manchanda@gmail.com>
2026-07-12 15:05:54 +0530

    Add: interleaving-string - submission-3

---
+++ Data Structures & Algorithms/interleaving-string/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    bool solve(int idx1, int idx2, int idx3, string s1, string s2, string s3, vector<vector<vector<int>>>& dp) {
+        if (idx3 == s3.size() && idx1 == s1.size() && idx2 == s2.size()) return true;
+        if (idx1 == s1.size() && idx2 == s2.size()) return false;
+
+        if (dp[idx1][idx2][idx3] != -1) return dp[idx1][idx2][idx3];
+
+        bool match1 = false, match2 = false;
+        if (idx1 < s1.size() && s1[idx1] == s3[idx3]) {
+            match1 = solve(idx1 + 1, idx2, idx3 + 1, s1, s2, s3, dp);
+        }
+
+        if (idx2 < s2.size() && s2[idx2] == s3[idx3]) {
+            match2 = solve(idx1, idx2 + 1, idx3 + 1, s1, s2, s3, dp);
+        }
+
+        if (!match1 && !match2) {
+            return dp[idx1][idx2][idx3] = false;
+        }
+        return dp[idx1][idx2][idx3] = true;
+    }
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.size();
+        int m = s2.size();
+        int t = s3.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (m+1, vector<int> (t + 1, -1)));
+        return solve(0, 0, 0, s1, s2, s3, dp);
+    }
+};
